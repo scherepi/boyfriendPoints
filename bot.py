c076e167ec9f696d78e5987a9ea78fdb6c66fa49
scherepi <48649270+scherepi@users.noreply.github.com>
2024-12-05 17:56:17 -0600

    first commit

---
+++ bot.py
@@ -0,0 +1,14 @@
+import discord
+
+class myClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged in as {self.user}')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = myClient(intents=intents)
+client.run("my token")
